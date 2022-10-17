a975c5591c18cce87f71d00eb5ce76918d37c24f
Oscar Arámbula <oscar.arambula4388@alumnos.udg.mx>
2022-10-16 23:52:43 -0500

    Ahora se puede configurar desde un archivo el procesamiento

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 import pandas as pd
-from damimo import ZeroR, Tester
+from damimo import ZeroR, OneR, NaiveBayes, Tester
+import json
+
+
+with open("config.json", "r") as jsonfile:
+    config = json.load(jsonfile)
 
 
 def main():
+    columns = config["attributes"]
+    columns.append(config["class"])
+
     # Cargar los datos
     df = pd.read_csv(
-        "db.csv",
-        skiprows=[0],
-        names=["Compra", "Mantenimiento", "Puertas", "Personas", "Clase"],
+        config["data_set_path"],
+        skiprows=config["skip_rows"],
+        names=columns,
     )
 
-    # Creamos el modelo (para one-r y naive bayes puede haber mas parametros)
-    zr = ZeroR("Clase")
+    # Creamos el modelo
+    if config["model"] == "cero-r":
+        model = ZeroR(config["class"])
+    elif config["model"] == "one-r":
+        model = OneR(config["class"])
+    else:
+        if "numeric_attributes" in config:
+            na = config["numeric_attributes"]
+        else:
+            na = []
+        model = NaiveBayes(config["class"], na)
 
     # Instanciamos el tester.
     #   Le pasamos el modelo
     #   Y el conjunto de datos completo
-    tester = Tester(zr, df)
+    tester = Tester(model, df)
 
     # Ejecutamos las pruebas.
-    #   Indicamos que tome el 70% para conjunto de entrenamiento.
-    #   Y que repita el entrenamiento 50 veces (en cero-r solo se necesita 1)
-    result = tester.test(0.7, 1000)
+    result = tester.test(config["test"]["train_frac"], config["test"]["times"])
 
     # Mostramos resultados (o lo vaciamos en un archivo)
-    print(result)
+    print("Aciertos", result[0])
+    print("Instancias", result[1])
+    print("Promedio", result[2])
 
 
 if __name__ == "__main__":
