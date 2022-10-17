f1745020ab12152e3d192703b7a33b319bfc2690
Oscar Arámbula <oscar.arambula4388@alumnos.udg.mx>
2022-10-16 21:28:04 -0500

    Implementado modelo CeroR

--- damimo/models/zero_r.py
+++ damimo/models/zero_r.py
@@ -7,11 +7,15 @@ from damimo.helpers import helper
 
 
 class ZeroR(Model):
+    frecuency_table: pd.Series
+    most_frecuent_class: object
+
     def __init__(self, class_col):
         self.class_col = class_col
 
     def train(self, data_set: pd.DataFrame):
-        pass
+        self.frecuency_table = helper.frecuency(data_set, self.class_col)
+        self.most_frecuent_class = self.frecuency_table.idxmax()
 
-    def predict(self, instance: pd.Series):
-        pass
+    def predict(self, instance: pd.Series = None):
+        return self.most_frecuent_class
